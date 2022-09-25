8260352575acff2ea5ae4eb08fd6b3eb1e23a689
prajwal-3-14159 <prajwal.k.314@gmail.com>
2022-09-25 18:39:19 +0530

    updates

---
+++ filter/codes/8pt_fft.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+
+
+#define PI 3.141592653589793238462
+
+int main()
+{
+    int i, j, k, n, m, l, p, q, r, s, t, u, v, w, x, y, z;
+    int N = 8;
+    double a[8][8], b[8][8], c[8][8], d[8][8], e[8][8], f[8][8], g[8][8], h[8][8];
+    double A[8][8], B[8][8], C[8][8], D[8][8], E[8][8], F[8][8], G[8][8], H[8][8];
+    double temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8;
+    double temp9, temp10, temp11, temp12, temp13, temp14, temp15, temp16;
+    double temp17, temp18, temp19, temp20, temp21, temp22, temp23, temp24;
+    double temp25, temp26, temp27, temp28, temp29, temp30, temp31, temp32;
+    double temp33, temp34, temp35, temp36, temp37, temp38, temp39, temp40;
+    double temp41, temp42, temp43, temp44, temp45, temp46, temp47, temp48;
+    double temp49, temp50, temp51, temp52, temp53, temp54, temp55, temp56;
+    double temp57, temp58, temp59, temp60, temp61, temp62, temp63, temp64;
+    
+    return 0;
+}
